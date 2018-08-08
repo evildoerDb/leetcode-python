e02ef76c5df73be509fadc2c787d893e91fe7dff
evildoerDb <179453684@qq.com>
2018-08-08 23:38:27 +0800

    init

---
+++ com.bobo/easy/toLowerCase.py
@@ -0,0 +1,29 @@
+"""
+Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+
+
+Example 1:
+
+Input: "Hello"
+Output: "hello"
+Example 2:
+
+Input: "here"
+Output: "here"
+Example 3:
+
+Input: "LOVELY"
+Output: "lovely"
+
+709  转换小写
+"""
+
+def toLowerCase(self, str):
+    """
+    :type str: str
+    :rtype: str
+    """
+    return str.lower()
+
+
